e8d5ea7ecc7191a9d35519e61de562b1fb567270
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-04-14 20:28:05 +0530

    Day-3 14-04-2023

---
+++ Welcome.java
@@ -0,0 +1,29 @@
+class Welcome
+{
+	int marks=222;//global variable
+public static void main(String args[])
+{
+	byte age=30;//local variable
+	float salary=23.56523223585f;
+	double salary1=33.987654321223;
+	char gender='M';
+	boolean result=true;
+	System.out.println(salary+"     "+salary1+"   "+age+" "+gender);
+	
+System.out.println("welcome to java");
+
+}
+
+
+}
+//user understandable language  --->JVM
+	
+//binary --0's 1's 
+
+//source code --->byte code 
+
+//.java --->.class  JVM
+
+//compailation--->compiler  -->javac filename.java
+
+//execution		-->JVM-->.class-->java classname-->output
